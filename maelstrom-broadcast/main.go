f4eee2fcfbf235da8ca135fbce81b79506715376
paul-kang-1 <loiter97@gmail.com>
2023-05-30 01:27:41 -0400

    Completed section 3a

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -7,12 +7,18 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+var db map[int]struct{}
+
 func handle_broadcast(n *maelstrom.Node) maelstrom.HandlerFunc {
 	return func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
+		message := int(body["message"].(float64))
+		db[message] = struct{}{}
+		delete(body, "message")
+		body["type"] = "broadcast_ok"
 		return n.Reply(msg, body)
 	}
 }
@@ -23,6 +29,14 @@ func handle_read(n *maelstrom.Node) maelstrom.HandlerFunc {
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
+		res := make([]int, len(db))
+		i := 0
+		for k := range db {
+			res[i] = k
+			i++
+		}
+		body["messages"] = res
+		body["type"] = "read_ok"
 		return n.Reply(msg, body)
 	}
 }
@@ -33,12 +47,15 @@ func handle_topology(n *maelstrom.Node) maelstrom.HandlerFunc {
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
+		body["type"] = "topology_ok"
+		delete(body, "topology")
 		return n.Reply(msg, body)
 	}
 }
 
 func main() {
 	n := maelstrom.NewNode()
+	db = make(map[int]struct{})
 
 	// Register handlers
 	n.Handle("broadcast", handle_broadcast(n))
---
+++ maelstrom-broadcast/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MAELSTROM_BIN="../maelstrom/maelstrom"
+if [ ! -f $MAELSTROM_BIN ]; then
+	echo "Maelstrom executable does not exist in ${MAELSTROM_BIN}."
+	exit 1
+fi
+
+go get github.com/jepsen-io/maelstrom/demo/go
+go install .
+
+$MAELSTROM_BIN test \
+	-w broadcast \
+	--bin ~/go/bin/maelstrom-broadcast \
+	--node-count 1 \
+	--time-limit 20 \
+	--rate 10
