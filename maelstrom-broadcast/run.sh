88cc91a1169e5552992246fe8157e82a08883b4e
paul-kang-1 <loiter97@gmail.com>
2023-05-31 02:10:49 -0400

    added per-stage script for stage 3

--- maelstrom-broadcast/run.sh
+++ maelstrom-broadcast/run.sh
@@ -9,9 +9,36 @@ fi
 go get github.com/jepsen-io/maelstrom/demo/go
 go install .
 
-$MAELSTROM_BIN test \
-	-w broadcast \
-	--bin ~/go/bin/maelstrom-broadcast \
-	--node-count 1 \
-	--time-limit 20 \
-	--rate 10
+part_a() {
+	echo "💚 Running part a"
+	$MAELSTROM_BIN test \
+		-w broadcast \
+		--bin ~/go/bin/maelstrom-broadcast \
+		--node-count 1 \
+		--time-limit 20 \
+		--rate 10
+}
+
+part_b () {
+	echo "💚 Running part b"
+	$MAELSTROM_BIN test \
+		-w broadcast \
+		--bin ~/go/bin/maelstrom-broadcast \
+		--node-count 5 \
+		--time-limit 20 \
+		--rate 10
+}
+
+if [[ $# -ge 2 ]]; then echo "usage: $0 [<part>]" >&2; exit 1; fi
+
+if [[ $# -eq 0 ]]; then
+	part_a
+	part_b
+	exit 0
+fi
+
+case "$1" in
+	a) part_a ;;
+	b) part_b ;;
+	*) echo "💔 invalid part number" >&2; exit 1 ;;
+esac
