c9c0020edb0c2ce3ab8f5c92d187faadf9d4f645
paul-kang-1 <loiter97@gmail.com>
2023-05-31 01:28:17 -0400

    completed challenge 3a

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 var db map[int]struct{}
+var topology map[string][]string
 
 func handle_broadcast(n *maelstrom.Node) maelstrom.HandlerFunc {
 	return func(msg maelstrom.Message) error {
@@ -16,7 +18,14 @@ func handle_broadcast(n *maelstrom.Node) maelstrom.HandlerFunc {
 			return err
 		}
 		message := int(body["message"].(float64))
-		db[message] = struct{}{}
+		if _, ok := db[message]; !ok {
+			db[message] = struct{}{}
+			for _, neighbor := range topology[n.ID()] {
+			// NOTE: Use below to ignore provided topology, assuming full connection
+			// for _, neighbor := range n.NodeIDs() {
+				n.Send(neighbor, body)
+			}
+		}
 		delete(body, "message")
 		body["type"] = "broadcast_ok"
 		return n.Reply(msg, body)
@@ -41,6 +50,24 @@ func handle_read(n *maelstrom.Node) maelstrom.HandlerFunc {
 	}
 }
 
+func get_topology(body *map[string]any) (map[string][]string, error) {
+	field, ok := (*body)["topology"].(map[string]any)
+	if !ok {
+		return nil, fmt.Errorf("Invalid JSON body format: 'topology'")
+	}
+	topology := make(map[string][]string)
+	for node, neighbors := range field {
+		if neighborsJSON, ok := neighbors.([]any); ok {
+			for _, neighbor := range neighborsJSON {
+				if neighborStr, ok := neighbor.(string); ok {
+					topology[node] = append(topology[node], neighborStr)
+				}
+			}
+		}
+	}
+	return topology, nil
+}
+
 func handle_topology(n *maelstrom.Node) maelstrom.HandlerFunc {
 	return func(msg maelstrom.Message) error {
 		var body map[string]any
@@ -48,6 +75,11 @@ func handle_topology(n *maelstrom.Node) maelstrom.HandlerFunc {
 			return err
 		}
 		body["type"] = "topology_ok"
+		top, err := get_topology(&body)
+		if err != nil {
+			return err
+		}
+		topology = top
 		delete(body, "topology")
 		return n.Reply(msg, body)
 	}
@@ -56,6 +88,7 @@ func handle_topology(n *maelstrom.Node) maelstrom.HandlerFunc {
 func main() {
 	n := maelstrom.NewNode()
 	db = make(map[int]struct{})
+	topology = make(map[string][]string)
 
 	// Register handlers
 	n.Handle("broadcast", handle_broadcast(n))
